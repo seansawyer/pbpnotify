6900c775ef3c4169b5d51b8b9da5bcab0d1e7464
Sean Sawyer <ssawyer@pandora.com>
2019-09-13 22:35:09 -0400

    Move sub name an recipients to arguments

--- pbpnotify.py
+++ pbpnotify.py
@@ -1,16 +1,21 @@
+import argparse
 import os
 import requests
 import smtplib
 import ssl
+import sys
 
-SUB_NAME = 'pbpmooo'
-SUB_NEW_URL_FORMAT = 'https://oauth.reddit.com/r/{sub}/new.json'
 USER_AGENT = 'pbpmooo-notify/0.1 by ikvind'
 TOKEN_VAR = 'REDDIT_TOKEN'
 
-def run():
+def run(args):
+    if args.email is None:
+        print('No emails to notify. Please supply --email at least once.')
+        sys.exit(1)
+    sub = args.subreddit
+    to_addrs = args.email
     token = os.getenv('REDDIT_TOKEN')
-    new_url = SUB_NEW_URL_FORMAT.format(sub=SUB_NAME)
+    new_url = f'https://oauth.reddit.com/r/{sub}/new.json'
     headers = {
         'Authorization': f'bearer {token}',
         'User-Agent': USER_AGENT,
@@ -22,9 +27,9 @@ def run():
         return
     data = response.json()
     posts = [child['data'] for child in data['data']['children']]
-    send_email(posts)
+    send_email(to_addrs, posts)
 
-def send_email(posts):
+def send_email(to_addrs, posts):
     user = os.getenv('GMAIL_USER')
     password = os.getenv('GMAIL_PASSWORD')
     port = 465  # For SSL
@@ -40,12 +45,16 @@ def send_email(posts):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
         server.login(user, password)
-        to_addrs = [
-            'mikezackles@gmail.com',
-            'seancsawyer@gmail.com',
-        ]
         server.sendmail(user, to_addrs, message)
+
+def build_arg_parser():
+    parser = argparse.ArgumentParser(description='Send notifications of new posts to a private subreddit.')
+    parser.add_argument('subreddit', metavar='SUB', help='the subreddit name')
+    parser.add_argument('--email', action='append')
+    return parser
 
 
 if __name__ == '__main__':
-    run()
+    parser = build_arg_parser()
+    args = parser.parse_args()
+    run(args)
